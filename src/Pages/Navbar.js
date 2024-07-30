aa7a3da9b19c48f2d1b3bca95b1a3ec3218f1fe9
Prakash-2002 <77295376+Prakash-2002@users.noreply.github.com>
2024-07-30 15:20:54 +0530

    Update Navbar.js

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,36 +1,3 @@
-/*import React from "react";
-import "././Navbar.css";
-import MenuToggle from "./MenuToggle";
-
-const Navbar = () => {
-  return (
-    
-     
-      <nav className="navbar">
-      <div>
-        <MenuToggle />
-      </div>
-
-        <div className="navbar-logo">
-          KUKU<span>FM</span>
-        </div>
-        <div className="navbar-items">
-          <input
-            type="text"
-            className="navbar-search"
-            placeholder="Search Audiobook and Stories"
-          />
-          <div className="navbar-link">Buy Premium</div>
-          <div className="navbar-link">Buy Coins</div>
-          <button className="navbar-button">Log In</button>
-        </div>
-      </nav>
-    
-  );
-};
-
-export default Navbar;
-*/
 import React from "react";
 import "./Navbar.css";
 import MenuToggle from "./MenuToggle";
